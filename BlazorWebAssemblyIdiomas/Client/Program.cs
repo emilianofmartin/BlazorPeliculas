3285a2a26ca248adc99b8cba4445a7274dfb627a
Emiliano F. Martín <emartin@softland.com.ar>
2023-02-23 13:00:16 -0300

    WebAssemblyIdiomas

--- BlazorWebAssemblyIdiomas/Client/Program.cs
+++ BlazorWebAssemblyIdiomas/Client/Program.cs
@@ -1,11 +1,25 @@
 using BlazorWebAssemblyIdiomas.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.JSInterop;
+using System.Globalization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddLocalization();
+
+var host = builder.Build(); //Necesitamos una ref de IJSRuntime
+var js = host.Services.GetRequiredService<IJSRuntime>();
+var culture = await js.InvokeAsync<string>("culture.get");
+var thisCulture = new CultureInfo("en-US");
+
+if(culture != null)
+    thisCulture = new CultureInfo(culture);
+
+CultureInfo.DefaultThreadCurrentCulture = thisCulture;
+CultureInfo.DefaultThreadCurrentUICulture = thisCulture;
 
 await builder.Build().RunAsync();
---
+++ BlazorWebAssemblyIdiomas/Shared/Person.cs
@@ -0,0 +1,16 @@
+﻿using BlazorWebAssemblyIdiomas.Shared.Resources;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWebAssemblyIdiomas.Shared {
+    public class Person {
+        [Required(
+            ErrorMessageResourceType = typeof(Resource), 
+            ErrorMessageResourceName = nameof(Resource.required))]
+        public string Name { get; set; } = null!;
+    }
+}
